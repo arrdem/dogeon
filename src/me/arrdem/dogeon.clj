a90c36d2255495852adac39cdfefdb0219e93c9d
arrdem <me@arrdem.com>
2014-06-08 00:34:34 -0500

    First cut at the library & tests

--- src/me/arrdem/dogeon.clj
+++ src/me/arrdem/dogeon.clj
@@ -1,6 +1,83 @@
-(ns me.arrdem.dogeon)
+(ns me.arrdem.dogeon
+  (:require [instaparse.core :as insta]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def dogeon-parser
+  (insta/parser
+   (slurp "doge.bnf")))
+
+(defn read8 [& digits]
+  (reduce (fn [acc d] (+ d (* 8 acc)))
+          0 digits))
+
+(defn make-number
+  [{:keys [num-part
+           frac-part
+           expt-part]
+    :or {num-part 0
+         frac-part 0
+         expt-part 1}
+    :as args}]
+  (* expt-part
+     (+ frac-part
+        num-part)))
+
+(def dogeon-rewrites
+  {:string (fn [& nodes]
+             (->> nodes (map second) (reduce str)))
+
+   :object (fn [& kvs]
+             (into {} kvs))
+
+   :pair   (fn [k v]
+             [k v])
+
+   :value  (fn [x]
+             (cond (= "yes" x)   true
+                   (= "no" x)    false
+                   (= "empty" x) nil
+                   true          x))
+
+   :array (fn [& m0ar]
+            (vec m0ar))
+
+   :int   identity
+
+   :frac  (fn [_dot & digits]
+             {:frac-part (/ (apply read8 digits)
+                            (reduce * (repeat (count digits) 8.0)))})
+
+   :exp   (fn [_very & digits]
+            (let [expt (apply read8 digits)]
+              {:expt-part (reduce * (repeat expt 8))}))
+   
+   :negative (fn [x]
+               (* -1 x))
+
+   :positive (fn [& parts]
+               (->> parts
+                    (reduce merge)
+                    make-number))
+
+   :digit1-7 (fn [x]
+               (Integer/parseInt x))
+
+   :digit    (fn [x]
+               (Integer/parseInt x))
+
+   :digits   (fn [& m0ar]
+               {:num-part (apply read8 m0ar)})
+
+   :escaped-char (fn [_ c]
+                   (case c
+                     ("n")  "\n"
+                     ("b")  "\b"
+                     ("t")  "\t"
+                     ("\"") "\""
+                     ("f")  "\f"
+                     ("r")  "\r"))})
+
+(defn read-dogeon
+  [input-string]
+  (->> input-string
+       dogeon-parser
+       (insta/transform dogeon-rewrites)))
--- test/me/arrdem/dogeon_test.clj
+++ test/me/arrdem/dogeon_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [me.arrdem.dogeon :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parser-tests
+  (is (= (read-dogeon "such \"foo\" is so \"bar\" also \"baz\" and \"fizzbuzz\" many wow")
+         {"foo" ["bar" "baz" "fizzbuzz"]}))
+
+  (is (= (read-dogeon "such \"foo\" is \"bar\" wow")
+         {"foo" "bar"}))
+  
+  (is (= (read-dogeon "such \" \\\"such \" is \"bar\" wow")
+         {" \"such " "bar"}))
+
+  (is (= (read-dogeon "such \"foo\" is such \"shiba\" is \"inu\", \"doge\" is yes wow wow")
+         {"foo" {"shiba" "inu", "doge" true}}))
+
+  (is (= (read-dogeon "such \"foo\" is 42 wow")
+         {"foo" 34}))
+
+  (is (= (read-dogeon "such \"foo\" is 42.3very3 wow")
+         {"foo" 17600.0})))
